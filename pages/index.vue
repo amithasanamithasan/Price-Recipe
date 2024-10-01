3573e9edad736fd33e0e02787a34bb3977baf1c6
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-10-01 13:54:25 +0600

    index.vue file nuxtlink params using /recipes/ indevisual card id  show

--- pages/index.vue
+++ pages/index.vue
@@ -60,9 +60,9 @@ const {data , error}= await useFetch<RecipeResponse>("https://dummyjson.com/reci
                 <span></span>
               </div>
             </div>
-            <button class="px-4 py-2 text-black self-start bg-amber-600 rounded-md text-base lg:text-lg cursor-pointer">
+            <NuxtLink :to="`/recipes/${recipe.id}`" class="px-4 py-2 text-black self-start bg-amber-600 rounded-md text-base lg:text-lg cursor-pointer">
               View
-            </button>
+            </NuxtLink>
           </div>
         </div>
       </div>
